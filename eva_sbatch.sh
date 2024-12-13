59722e12ce148f7c041f966166971f26756f4d5b
Jinfan Liu <1051666563@qq.com>
2024-12-13 14:53:23 +0800

    Add eva_sbatch

---
+++ eva_sbatch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --job-name=kv-eva
+#SBATCH --output=kv-eva_%A.out
+#SBATCH --error=kv-eva_%A.err
+#SBATCH --time=30
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jinfan@comp.nus.edu.sg
+
+. .venv/bin/activate
+srun python eval_longbench.py
