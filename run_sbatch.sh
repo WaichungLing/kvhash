385ae5defe20a2ce922364ba188ea5fe61bea867
Jinfan Liu <1051666563@qq.com>
2024-12-14 15:31:46 +0800

    Add run_sbatch

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -2,10 +2,32 @@
 #SBATCH --job-name=kv
 #SBATCH --output=kv_%A.out
 #SBATCH --error=kv_%A.err
-#SBATCH --time=300
+#SBATCH --time=600
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jinfan@comp.nus.edu.sg
 #SBATCH --gres=gpu:a100-80:1
 
 . .venv/bin/activate
-srun python run_longbench.py --cache_budget=0.4
+
+run() {
+  srun python run_longbench.py --cache_budget=$1 >kv_$1.out 2>kv_$1.err
+}
+
+eva() {
+  srun python eval_longbench.py --cache_budget=$1 >ev_$1.out 2>ev_$1.err
+}
+
+run 1.0
+eva 1.0
+
+run 0.8
+eva 0.8
+
+run 0.6
+eva 0.6
+
+run 0.4
+eva 0.4
+
+run 0.2
+eva 0.2
