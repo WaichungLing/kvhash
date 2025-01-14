854ec2f530d31888c8541f02263f1a5e53f7a744
Jinfan Liu <1051666563@qq.com>
2025-01-14 13:49:19 +0800

    Add source env

--- single_batch.sh
+++ single_batch.sh
@@ -5,6 +5,7 @@
 #SBATCH --time=800
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jinfan@u.nus.edu
-#SBATCH --gres=gpu:a100-80:1
+#SBATCH --gres=gpu:a100-40:1
 
+. .venv/bin/activate
 srun python run_longbench.py >kv.out 2>kv.err
