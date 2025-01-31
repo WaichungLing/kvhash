c849c29720fe627b4859fc35585030271fa6f9fe
Jinfan Liu <chrisckeyliu@gmail.com>
2025-01-31 11:40:22 +0800

    Change Email

--- single_sbatch.sh
+++ single_sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=kv_%A.err
 #SBATCH --time=500
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=wling@comp.nus.edu.sg
+#SBATCH --mail-user=jinfan@comp.nus.edu.sg
 #SBATCH --gres=gpu:a100-80:1
 
 # Run the training script
@@ -12,20 +12,20 @@
 srun python run_longbench.py \
     --enable_eviction \
     --model_name="meta-llama/Llama-3.2-3B-Instruct" \
-    --recent_protect_budget=64  \
+    --recent_protect_budget=64 \
     --cache_budget=512 \
     --proxy_total=64 \
     --proxy_latest=32 \
     --n_recursion=-1 \
-    --task="all" > kv.out 2> kv.err
+    --task="all" >kv.out 2>kv.err
 
 # Run the evaluation script
 # add --enable_eviction if needed
 srun python eval_longbench.py \
     --enable_eviction \
     --model_name="meta-llama/Llama-3.2-3B-Instruct" \
-    --recent_protect_budget=64  \
+    --recent_protect_budget=64 \
     --cache_budget=512 \
     --proxy_total=64 \
     --proxy_latest=32 \
-    --n_recursion=-1 > ev.out 2> ev.err
+    --n_recursion=-1 >ev.out 2>ev.err
